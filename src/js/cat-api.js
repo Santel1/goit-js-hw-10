0b9ec9207a30a614b0bda1ec1eb2cda4b34c1cf1
Santel1 <126976920+Santel1@users.noreply.github.com>
2023-08-15 13:56:06 +0200

    10

--- src/js/cat-api.js
+++ src/js/cat-api.js
@@ -1,4 +1,14 @@
-import { fetchBreeds, fetchCatByBreed } from './index.js';
+import axios from 'axios';
 
-fetchBreeds();
-fetchCatByBreed(breedId);
+axios.defaults.headers.common['x-api-key'] =
+  'live_WHAhnz5ysNhKVYTG2ZNyRSByETUIxcZv1tQr6y3rQy9beUbIwbAMDyvbcu21qK2Y';
+
+export function fetchBreeds() {
+  return axios.get('https://api.thecatapi.com/v1/breeds');
+}
+
+export function fetchCatByBreed(breedId) {
+  return axios.get(
+    `https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`
+  );
+}
--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,8 @@
-import axios from 'axios';
 import SlimSelect from 'slim-select';
 import 'slim-select/dist/slimselect.css';
 import { Report } from 'notiflix/build/notiflix-report-aio';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
-
-axios.defaults.headers.common['x-api-key'] =
-  'live_WHAhnz5ysNhKVYTG2ZNyRSByETUIxcZv1tQr6y3rQy9beUbIwbAMDyvbcu21qK2Y';
+import { fetchBreeds, fetchCatByBreed } from './cat-api';
 
 const refs = {
   breedSelect: document.querySelector('.breed-select'),
@@ -16,50 +13,51 @@ const refs = {
 
 refs.error.classList.add('js-loader');
 
-export function fetchBreeds() {
-  axios
-    .get('https://api.thecatapi.com/v1/breeds')
-    .then(resp => {
-      //   refs.breedSelect.innerHTML = createMurkupSelect(resp.data);
-      refs.breedSelect.insertAdjacentHTML(
-        'beforeend',
-        createMurkupSelect(resp.data)
-      );
-      slim();
-    })
-    .catch(err => {
-      Report.failure(
-        'Oops! Something went wrong! Try reloading the page!',
-        `${err}`,
-        'Okay'
-      );
-      refs.error.classList.remove('js-loader');
-      refs.breedSelect.classList.add('js-loader');
-    });
-}
+fetchBreeds()
+  .then(resp => {
+    refs.breedSelect.insertAdjacentHTML(
+      'beforeend',
+      createMurkupSelect(resp.data)
+    );
+    slim();
+  })
+  .catch(err => {
+    Report.failure(
+      'Oops! Something went wrong! Try reloading the page!',
+      `${err}`,
+      'Okay'
+    );
+    refs.error.classList.remove('js-loader');
+    refs.breedSelect.classList.add('js-loader');
+  });
 
-export function fetchCatByBreed(breedId) {
-  axios
-    .get(`https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`)
-    .then(resp => {
-      if (resp.data.length === 0) {
-        Notify.failure("Sorry,we haven't information about this breed.");
-      }
-      refs.loader.classList.add('js-loader');
-      refs.catInfo.classList.remove('js-loader');
-      refs.breedSelect.classList.remove('js-loader');
-      refs.catInfo.innerHTML = createMurkupCatsInfo(resp.data);
-    })
-    .catch(err => {
-      Report.failure(
-        'Oops! Something went wrong! Try reloading the page!',
-        `${err}`,
-        'Okay'
-      );
-      refs.error.classList.remove('js-loader');
-      refs.breedSelect.classList.add('js-loader');
-    });
-}
+refs.breedSelect.addEventListener('change', event => {
+  const breedId = event.target.value;
+  refs.loader.classList.remove('js-loader');
+  refs.catInfo.classList.add('js-loader');
+  refs.breedSelect.classList.add('js-loader');
+  if (breedId) {
+    fetchCatByBreed(breedId)
+      .then(resp => {
+        if (resp.data.length === 0) {
+          Notify.failure("Sorry,we haven't information about this breed.");
+        }
+        refs.loader.classList.add('js-loader');
+        refs.catInfo.classList.remove('js-loader');
+        refs.breedSelect.classList.remove('js-loader');
+        refs.catInfo.innerHTML = createMurkupCatsInfo(resp.data);
+      })
+      .catch(err => {
+        Report.failure(
+          'Oops! Something went wrong! Try reloading the page!',
+          `${err}`,
+          'Okay'
+        );
+        refs.error.classList.remove('js-loader');
+        refs.breedSelect.classList.add('js-loader');
+      });
+  }
+});
 
 function createMurkupSelect(arr) {
   return arr
@@ -78,16 +76,6 @@ function createMurkupCatsInfo(data) {
     )
     .join('');
 }
-
-refs.breedSelect.addEventListener('change', event => {
-  const breedId = event.target.value;
-  refs.loader.classList.remove('js-loader');
-  refs.catInfo.classList.add('js-loader');
-  refs.breedSelect.classList.add('js-loader');
-  if (breedId) {
-    fetchCatByBreed(breedId);
-  }
-});
 
 function slim() {
   new SlimSelect({
